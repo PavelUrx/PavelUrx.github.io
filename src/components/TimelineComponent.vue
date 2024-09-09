9632775eae51f5e1e22f72147406a46ac599c910
Pavel Urx <pavel.urx@protonmail.com>
2024-09-09 20:22:44 +0200

    UI elements of timeline settings

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -2,9 +2,38 @@
     <div class="bg-primary">
         <div class="container-sm p-5">
             <h2 class="text-center display-2 text-white">Časová osa</h2>
+            
+            <div class="row">
+                <p>Nastavení osy</p>
+                
+                <div class="d-flex justify-content-between py-1">
+                    <button @click="toggleSortOrder" class="btn btn-sm btn-secondary">
+                        {{ sortOrder === 'asc' ? 'Sort Ascending' : 'Sort Descending' }}
+                    </button>
+                </div>
+ 
+                <div class="d-flex justify-content-between">
+                    <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="displayEdu" v-model="displayEducation" :class="{ 'bg-secondary': displayEducation }">
+                    <label class="form-check-label" for="displayEdu">Vzdělání</label>
+                    </div>
+
+                    <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="displayRepos" v-model="displayRepos" :class="{ 'bg-secondary': displayRepos }">
+                    <label class="form-check-label" for="displayRepos">GitHub repozitáře</label>
+                    </div>
+
+                    <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="displayOtherProjects" v-model="displayOtherProjects" :class="{ 'bg-secondary': displayOtherProjects }">
+                    <label class="form-check-label" for="displayOtherProjects">Ostatní projekty</label>
+                    </div>
+                </div>
+
+            </div>
+
             <div>
                 <div v-if="repositories.length > 0">
-                    <TimelineContentComponent 
+                    <TimelineContentComponent  
                         v-for="repo in repositories" 
                         :key="repo.name" 
                         :data="repo" 
@@ -24,6 +53,10 @@ export default {
     data() {
         return {
             repositories: [],
+            displayEducation: true,
+            displayRepos: true,
+            displayOtherProjects: true,
+            sortOrder: 'desc',
         }
     },
     components: {
@@ -35,6 +68,11 @@ export default {
         await timelineController.fetchEducation();
         timelineController.orderByStart();
         this.repositories = timelineController.getTimelineContent();
+    },
+    methods: {
+        toggleSortOrder() {
+            this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+        }
     }
 }
 </script>
